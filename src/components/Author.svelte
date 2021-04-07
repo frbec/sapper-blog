9d60a53f6ebbe8e990131d57060a671c1c8e7cae
Fredrik Beckius <fredrikbeckius@gmail.com>
2021-04-07 17:48:34 +0200

    edit profile section

--- src/components/Author.svelte
+++ src/components/Author.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <style>
-    figure {
-        margin: 0
-    }
-    img {
-        width: 72px;
-        height: 72px;
-        border-radius: 6px;
-    }
+  figure {
+    margin: 0;
+  }
+  img {
+    width: 72px;
+    height: 72px;
+    border-radius: 6px;
+  }
   h3 {
     font-size: 1.4em;
     font-weight: 700;
@@ -20,11 +20,18 @@
     font-size: 0.9em;
     margin: 0 0 0.5em;
   }
-
 </style>
 
 <figure>
-<img src="/uploads/fredrik_copy.jpg" alt="Fredrik smiling wearing a grey t-shirt">
+  <a href="/uploads/fredrik.jpg">
+    <img
+      src="/uploads/fredrik_copy.jpg"
+      alt="Fredrik smiling wearing a grey t-shirt" />
+  </a>
 </figure>
 <h3>Fredrik Beckius</h3>
-<p>I believe that the world needs changing from the current trajectory. My intentions is to spread a vision of a better world, with equality, sustainability and technology as a helper, instead of a ruler.</p>
+<p>
+  I believe that the world needs changing from the current trajectory. My
+  intention is to spread a vision of a better world, with equality,
+  sustainability and technology as a helper, instead of a ruler.
+</p>
